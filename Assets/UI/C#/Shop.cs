1c354712ca206ac76dc3f59a7087e467476c4084
Sadnizm <sadaf.nzm@gmail.com>
2025-10-04 14:19:28 -0400

    Description Box

--- Assets/UI/C#/Shop.cs
+++ Assets/UI/C#/Shop.cs
@@ -2,34 +2,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-
 public class Shop_UI : MonoBehaviour
 {
+    [SerializeField] int inventoryLimit = 12;
+    [SerializeField] float money = 1500f;
+
+    [System.Serializable]
+    public class ListItem
+    {
+        public string itemName;
+        public float itemPrice;
+        [TextArea] public string itemDescription;
+    }
+
     [SerializeField] UIDocument ui;
+
     // Player 1 (left)
-    [SerializeField] List<string> ShopItems1 = new();
-    [SerializeField] List<string> InventoryItems1 = new();
+    [SerializeField] List<ListItem> ShopItems1 = new();
+    [SerializeField] List<ListItem> InventoryItems1 = new();
 
     // Player 2 (right)
-    [SerializeField] List<string> ShopItems2 = new();
-    [SerializeField] List<string> InventoryItems2 = new();
-
-    [SerializeField] int inventoryLimit = 12;
+    [SerializeField] List<ListItem> ShopItems2 = new();
+    [SerializeField] List<ListItem> InventoryItems2 = new();
 
     void OnEnable()
     {
         var root = ui.rootVisualElement;
 
-        // Left side (P1)
         SetupSide(root, "ShopList_1", "Inventory_1", ShopItems1, InventoryItems1);
-
-        // Right side (P2)
         SetupSide(root, "ShopList_2", "Inventory_2", ShopItems2, InventoryItems2);
 
+        var moneyLabel = root.Q<Label>("MoneyLabel");
+        if (moneyLabel != null) moneyLabel.text = $"{money} $";
     }
 
     void SetupSide(VisualElement root, string shopPanelName, string invPanelName,
-               List<string> shopItems, List<string> invItems)
+                   List<ListItem> shopItems, List<ListItem> invItems)
     {
         var shopPanel = root.Q<VisualElement>(shopPanelName);
         var invPanel = root.Q<VisualElement>(invPanelName);
@@ -37,31 +45,66 @@ public class Shop_UI : MonoBehaviour
         shopPanel.Clear();
         invPanel.Clear();
 
-        // preload inventory
-        foreach (var name in invItems)
-            AddInventoryButton(name, invPanel);
+        // find labels in this player's column
+        var playerRoot = shopPanel.parent.parent;
+        var titleLabel = playerRoot.Q<Label>(className: "description-title");
+        var priceLabel = playerRoot.Q<Label>(className: "price-text");
+        var descLabel = playerRoot.Q<Label>(className: "description-text");
+
+        // preload inventory (not removable)
+        foreach (var item in invItems)
+            AddInventoryButton(item, invPanel, false, titleLabel, priceLabel, descLabel);
 
         // build shop
-        foreach (var name in shopItems)
+        foreach (var item in shopItems)
         {
-            var btn = new Button { text = name };
+            var btn = new Button { text = item.itemName, focusable = true };
             btn.AddToClassList("button");
+
+         
+            AttachInfoHandlers(btn, item, titleLabel, priceLabel, descLabel, isBuy: true);
 
             btn.clicked += () =>
             {
                 if (invPanel.childCount >= inventoryLimit) return;
-                AddInventoryButton(name, invPanel);
+                AddInventoryButton(item, invPanel, true, titleLabel, priceLabel, descLabel);
             };
 
             shopPanel.Add(btn);
         }
     }
 
-    void AddInventoryButton(string itemName, VisualElement invPanel)
+    void AddInventoryButton(ListItem item, VisualElement invPanel, bool removable,
+                            Label titleLabel, Label priceLabel, Label descLabel)
     {
-        var invBtn = new Button { text = itemName };
+        var invBtn = new Button { text = item.itemName, focusable = true };
         invBtn.AddToClassList("button");
-        invBtn.clicked += () => invPanel.Remove(invBtn); // remove on click
+
+       
+        AttachInfoHandlers(invBtn, item, titleLabel, priceLabel, descLabel, isBuy: false);
+
+        if (removable)
+            invBtn.clicked += () => invPanel.Remove(invBtn);
+
         invPanel.Add(invBtn);
+    }
+
+    
+    void AttachInfoHandlers(VisualElement ve, ListItem item,
+                            Label titleLabel, Label priceLabel, Label descLabel,
+                            bool isBuy)
+    {
+        System.Action update = () =>
+        {
+            if (titleLabel != null) titleLabel.text = item.itemName;
+            if (priceLabel != null)
+                priceLabel.text = isBuy
+                    ? $"Buy For -${item.itemPrice:0.##}"
+                    : $"Sell For +${item.itemPrice:0.##}";
+            if (descLabel != null) descLabel.text = item.itemDescription;
+        };
+
+        ve.RegisterCallback<MouseEnterEvent>(_ => update());
+        ve.RegisterCallback<FocusInEvent>(_ => update());   
     }
 }
