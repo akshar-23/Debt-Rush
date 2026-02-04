d3be834fd87a31373bb7af70eb1ba15d0b62dca8
sebastiansalas94 <sebastiansalas94@gmail.com>
2026-02-03 19:26:13 -0500

    Added TriGun Weapon

---
+++ Assets/Scripts/Items/TriGun.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.ProBuilder;
+
+public class TriGun : ShopItem
+{
+    [Header("TriGun Settings")]
+    [SerializeField] public float fireRate = 0.1f;
+    public GameObject triGunProjectile;
+    [SerializeField] float angleOffset = 15f;
+    [SerializeField] float sideOffset = 0.2f;
+
+    private float nextFireTime = 0f;
+
+    [SerializeField] AudioClip ShotgunSFX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentCount = maxCount;
+    }
+
+    public override void Execute()
+    {
+        if (isActiveItem)
+        {
+            PlayerController pc = GameManager.Instance.players[0].GetComponent<PlayerController>();
+
+            if (currentCount <= 0)
+            {
+                Debug.Log("Out of Ammo!");
+                pc.DeleteInventoryItem(this);
+                return;
+            }
+
+
+            if (ShotgunSFX != null)
+            {
+                AudioManager.Instance.PlaySFX(ShotgunSFX);
+            }
+
+            Vector3 spawnPos = pc.transform.position + pc.transform.forward * 2f;
+            
+            Vector3 leftPos = spawnPos - transform.right * sideOffset;
+            Vector3 midPos = spawnPos;
+            Vector3 rightPos = spawnPos + transform.right * sideOffset;
+
+            GameObject projLeft = Instantiate(triGunProjectile, leftPos, transform.rotation * Quaternion.Euler(0f, 0f, angleOffset));
+            GameObject proj = Instantiate(triGunProjectile, midPos, transform.rotation);
+            GameObject projRight = Instantiate(triGunProjectile, rightPos, transform.rotation * Quaternion.Euler(0f, 0f, -angleOffset));
+
+            Projectile pL = projLeft.GetComponent<Projectile>();
+            Projectile p = proj.GetComponent<Projectile>();
+            Projectile pR = projRight.GetComponent<Projectile>();
+
+
+            Vector3 baseDir = pc.transform.forward;
+            Vector3 dirLeft = Quaternion.AngleAxis(-angleOffset, Vector3.up) * baseDir;
+            Vector3 dirMid = baseDir;
+            Vector3 dirRight = Quaternion.AngleAxis(angleOffset, Vector3.up) * baseDir;
+
+            if (p != null)
+            {
+                pL.archetype = Archetype.Player;
+                p.archetype = Archetype.Player;
+                pR.archetype = Archetype.Player;
+
+                pL.playerId = 1;
+                p.playerId = 1;
+                pR.playerId = 1;
+
+                pL.Init(dirLeft);
+                p.Init(dirMid);
+                pR.Init(dirRight);
+
+            }
+
+            nextFireTime = Time.time + fireRate;
+            currentCount--;
+        }
+    }
+}
