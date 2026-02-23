347471dde4eeb13f5de90166e09ea3a06a819e3d
AKKI\axr23 <axr230102@gmail.com>
2026-02-23 03:14:44 -0500

    Fade in black screen effect

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,25 +1,58 @@
+using System.Collections; // Required for Coroutines
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
 public class GameOverScreen : MonoBehaviour
 {
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    
+    [Header("Fade Settings")]
+    public Image backgroundImage;
+    public float fadeDuration = 1.5f;
 
     private void Start()
     {
         restartButton.onClick.AddListener(RestartGame);
     }
 
+    private void OnEnable()
+    {
+        if (backgroundImage != null)
+        {
+            StartCoroutine(FadeInBackground());
+        }
+    }
+
+    private IEnumerator FadeInBackground()
+    {
+        Color color = backgroundImage.color;
+        color.a = 0f;
+        backgroundImage.color = color;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            
+            color.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            backgroundImage.color = color;
+
+            yield return null;
+        }
+    }
+
     public void RestartGame()
     {
         Time.timeScale = 1f;
 
-        GameManager.Instance.ResetState();
-        
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ResetState();
+        }
 
         SceneManager.LoadScene("UI_Scene");
     }
@@ -31,6 +64,6 @@ public class GameOverScreen : MonoBehaviour
 
     public void Reset()
     {
-
+        
     }
 }
